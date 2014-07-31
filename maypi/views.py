2917e73186b4d56406445e96f3ab2337120f4e1a
Jacob Sayles <jsayles@gmail.com>
2014-07-31 17:48:33 +0000

    Initial commit

---
+++ maypi/views.py
@@ -0,0 +1,9 @@
+from django.contrib.auth.models import User
+from django.contrib.sites.models import Site
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render
+
+def home(request):
+	return render(request, "home.html", {})
+
