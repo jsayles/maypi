c86da08bb253c8daa374f0dc5381b79601c4a38b
Jacob Sayles <jsayles@gmail.com>
2014-08-04 01:14:35 +0000

    now using a controller class

--- wiegand/daemon.py
+++ wiegand/daemon.py
@@ -15,19 +15,35 @@ def log(msg):
 	with open(LOGFILE, "a") as f:
 		f.write(time.ctime() + ": " + msg + "\n")
 
-def callback(bits, value):
-	log("key=%d" % value)
+class Controller:
+	def callback(self, bits, value):
+		if value == ESC:
+			log("<ESC>")
+		elif value == ENT:
+			log("<ENT>")
+		else:
+			log("key=%d" % value)
+
+	def start(self, d0pin, d1pin):
+		log("Starting wiegand daemon...")
+		self.pi = pigpio.pi()
+		self.w = wiegand.decoder(self.pi, d0pin, d1pin, self.callback)
+
+	def stop(self):
+		log("Stopping wiegand daemon...")
+		self.w.cancel()
+		self.pi.stop()
 
 if __name__ == "__main__":
-	log("Starting wiegand daemon...")
-	pi = pigpio.pi()
-	w = wiegand.decoder(pi, 14, 15, callback)
+	# Start our controller
+	controller = Controller()
+	controller.start(DATA0_PIN, DATA1_PIN)
 
 	try:
 		while True:
 			time.sleep(1)
 	except (KeyboardInterrupt, SystemExit):
-		log("Stopping wiegand daemon...")
-		w.cancel()
-		pi.stop()
+		controller.stop()
+		sys.exit(0)
+
 
