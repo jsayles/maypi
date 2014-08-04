7eaee09509b9f7a78785c99e0172787e03bc06cf
Jacob Sayles <jsayles@gmail.com>
2014-08-04 23:07:17 +0000

    Tweaking UI

--- maypi/urls.py
+++ maypi/urls.py
@@ -1,8 +1,16 @@
+import time
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    url(r'^$', 'maypi.views.home', name='home'),
-    url(r'^admin/', include(admin.site.urls)),
+	url(r'^robots\.txt$', lambda r: HttpResponse("User-agent: *\nDisallow: /", mimetype="text/plain")),
+	url(r'^cache\.manifest$', lambda r: HttpResponse(get_manifest(), mimetype="text/plain")),
+
+	url(r'^$', 'maypi.views.home', name='home'),
+	url(r'^admin/', include(admin.site.urls)),
 )
+
+def get_manifest():
+	return "CACHE MANIFEST\n#Time: %s\nCACHE:\nFALLBACK:\nNETWORK:\n*" % time.time()
+
